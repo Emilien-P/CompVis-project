089e53c3f673bbaa310f8cbbf9bcbc4d86567737
Emilien <emilien.pilloud@epfl.ch>
2018-05-22 13:19:24 -0700

    Initial commit - files from hw5

---
+++ models.py
@@ -0,0 +1,85 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import os
+import datetime
+import pdb
+import time
+import torchvision.models as torchmodels
+
+class BaseModel(nn.Module):
+    def __init__(self):
+        super(BaseModel, self).__init__()
+        if not os.path.exists('logs'):
+            os.makedirs('logs')
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S_log.txt')
+        self.logFile = open('logs/' + st, 'w')
+
+    def log(self, str):
+        print(str)
+        self.logFile.write(str + '\n')
+
+    def criterion(self):
+        return nn.MSELoss()
+
+    def optimizer(self):
+        return optim.SGD(self.parameters(), lr=0.001)
+
+    def adjust_learning_rate(self, optimizer, epoch, args):
+        lr = args.lr
+        lr *= (0.1) ** (epoch // 50)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+       
+
+
+class LazyNet(BaseModel):
+    def __init__(self):
+        super(LazyNet, self).__init__()
+        self.lin1 = nn.Linear(32 * 32 * 3, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 32*32*3)
+        x = self.lin1(x)
+        return x
+        
+
+class BoringNet(BaseModel):
+    def __init__(self):
+        super(BoringNet, self).__init__()
+        self.lin1 = nn.Linear(32 * 32 * 3, 120)
+        self.lin2 = nn.Linear(120, 84)
+        self.lin3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 32*32*3)
+        # without activation functions
+        '''x = self.lin1(x)
+        x = self.lin2(x)
+        x = self.lin3(x)'''
+        # with activation functions
+        x = F.relu(self.lin1(x))
+        x = F.relu(self.lin2(x))
+        x = F.relu(self.lin3(x))
+        return x
+
+
+class CoolNet(BaseModel):
+    def __init__(self):
+        super(CoolNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.lin1 = nn.Linear(16 * 5 * 5, 120)
+        self.lin2 = nn.Linear(120, 84)
+        self.lin3 = nn.Linear(84, 10)
+
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.lin1(x))
+        x = F.relu(self.lin2(x))
+        x = F.relu(self.lin3(x))
+        return x
